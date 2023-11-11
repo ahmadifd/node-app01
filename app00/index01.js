70d110dfa0344c07f68bf39e0d1ca29cafb20d6d
Farshid Ahmadi <ahmadi.fd@hotmail.com>
2023-11-11 17:59:49 +0330

    44

--- app00/index01.js
+++ app00/index01.js
@@ -1,3 +1,25 @@
 import jalali from "jalali-moment";
+import express from "express";
 
-console.log(jalali(new Date()).locale("fa").format("YYYY/MM/DD"));
+//console.log(jalali(new Date()).locale("fa").format("YYYY/MM/DD"));
+
+const app = express();
+
+app.get("/", (req, res) => {
+  //res.send("Hello");
+  console.log(req.query);
+  res.send([
+    { id: 1, name: "user1" },
+    { id: 2, name: "user2" },
+  ]);
+});
+
+app.get("/:id", (req, res) => {
+  console.log(req.params);
+  res.send({ id: req.params.id, name: `User${req.params.id}` });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`listen to ${PORT}`);
+});
